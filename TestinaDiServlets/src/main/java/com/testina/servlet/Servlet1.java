4c03e4f035d7f127676d61ade7de4ab3319e5b34
MassimoRovitti <33849488+MassimoRovitti@users.noreply.github.com>
2025-06-20 13:48:20 +0200

    Add files via upload

--- TestinaDiServlets/src/main/java/com/testina/servlet/Servlet1.java
+++ TestinaDiServlets/src/main/java/com/testina/servlet/Servlet1.java
@@ -22,215 +22,228 @@ import javax.servlet.http.HttpSession;
  *
  */
 public class Servlet1 extends HttpServlet {
-	private static final String MASSIMO1 = "Massimo1";
+    private static final String MASSIMO1 = "Massimo1";
 
-	/*
-	 **********************************
-	 **********************************
-	 * Metodi overrided
-	 **********************************
-	 */
+    /*
+     **********************************
+     **********************************
+     * Metodi overrided
+     **********************************
+     */
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Servlet1() {
-		customLog("Sono il costruttore public Servlet1() di Servlet1");
+        customLog("Sono il costruttore public Servlet1() di Servlet1");
     }
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void Init(
-		ServletConfig config
-	) throws ServletException {
-		customLog("Sono il costruttore public void Init(ServletConfig) di Servlet1");
-		super.init(config);
+    /**
+     * @see Servlet#init(ServletConfig)
+     */
+    public void Init(
+        ServletConfig config
+    ) throws ServletException {
+        customLog("Sono il costruttore public void Init(ServletConfig) di Servlet1");
+        super.init(config);
 
-		/* Eventual codice custom da qui in poi */
-	}
+        /* Eventual codice custom da qui in poi */
+    }
 
-	/**
-	 * @see Servlet#destroy()
-	 */
-	public void Destroy(
-	) {
-		customLog("Sono il distruttore public void Destroy() di Servlet1");
+    /**
+     * @see Servlet#destroy()
+     */
+    public void Destroy(
+    ) {
+        customLog("Sono il distruttore public void Destroy() di Servlet1");
 
-		/* Eventual codice custom da qui in poi */
-		super.destroy();
-	}
+        /* Eventual codice custom da qui in poi */
+        super.destroy();
+    }
 
-	/**
-	 * Processa la richiesta Head
-	 */
-	protected void doHead(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Head
+     */
+    protected void doHead(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Get
-	 */
-	protected void doGet(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Get
+     */
+    protected void doGet(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Post
-	 */
-	protected void doPost(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Post
+     */
+    protected void doPost(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Put
-	 */
-	protected void doPut(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Put
+     */
+    protected void doPut(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Delete
-	 */
-	protected void doDelete(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Delete
+     */
+    protected void doDelete(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Options
-	 */
-	protected void doOptions(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Options
+     */
+    protected void doOptions(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Trace
-	 */
-	protected void doTrace(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Trace
+     */
+    protected void doTrace(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta LastModified
-	 * questa pare che non passi da service(..., ...)
-	 * quindi dovrebbe chiamarla direttamente l'engine del web server
-	 */
-	protected long getLastModified(
-		HttpServletRequest request
-	) {
-		doMyOp(request, customLogl);
-		return 0;
-	}
-	
-	/**
-	 * Processa la richiesta all'internal service handler
-	 */
-	protected void service(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) {
-		/*
-			Ignoriamo il metodo standard cosi ci facciamo le richieste nostre
-			// super.service(request, response);
-		*/
-	    String method = request.getMethod();
-		DoLog("Sono il metodo protected void service(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet1");
-		
-		switch (method) {
-			case METHOD_HEAD :
-				doHead(request, response);
-				break;
-			case METHOD_GET :
-				doGet(request, response);
-				break;
-			case METHOD_POST :
-				doPost(request, response);
-				break;
-			case METHOD_PUT :
-				doPut(request, response);
-				break;
-			case METHOD_DELETE :
-				doDelete(request, response);
-				break;
-			case METHOD_OPTIONS :
-				doDelete(request, response);
-				break;
-			case METHOD_TRACE :
-				doDelete(request, response);
-				break;
-			case METHOD_MASSIMO1 :
-				doDelete(request, response);
-				break;
-			default:
-				customLog("Sono il metodo protected void service(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet1 e ... questo metodo non mi piace!");
+    /**
+     * Processa la richiesta LastModified
+     * questa pare che non passi da service(..., ...)
+     * quindi dovrebbe chiamarla direttamente l'engine del web server
+     */
+    protected long getLastModified(
+        HttpServletRequest request
+    ) {
+        doMyOp(request, customLogl);
+        return 0;
+    }
+    
+    /**
+     * Processa la richiesta all'internal service handler
+     */
+    protected void service(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        /*
+            Ignoriamo il metodo standard cosi ci facciamo le richieste nostre
+            // super.service(request, response);
+        */
+        String method = request.getMethod();
+        DoLog("Sono il metodo protected void service(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet1");
+        
+        switch (method) {
+            case METHOD_HEAD :
+                doHead(request, response);
+                break;
+            case METHOD_GET :
+                doGet(request, response);
+                break;
+            case METHOD_POST :
+                doPost(request, response);
+                break;
+            case METHOD_PUT :
+                doPut(request, response);
+                break;
+            case METHOD_DELETE :
+                doDelete(request, response);
+                break;
+            case METHOD_OPTIONS :
+                doDelete(request, response);
+                break;
+            case METHOD_TRACE :
+                doDelete(request, response);
+                break;
+            case METHOD_MASSIMO1 :
+                doDelete(request, response);
+                break;
+            default:
+                customLog("Sono il metodo protected void service(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet1 e ... questo metodo non mi piace!");
 
-				resp.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED, errMsg);
-				break;
-		}
-	}
-	
-	/**
-	 * Processa la richiesta al public service handler
-	 */
-	public void service(
-		ServletRequest request,
-		ServletResponse response
-	) {
-	    String method = request.getMethod();
-		customLog("Sono il metodo public void service(ServletRequest, ServletResponse) [" + method + "] di Servlet1");
+                resp.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED, errMsg);
+                break;
+        }
+    }
+    
+    /**
+     * Processa la richiesta al public service handler
+     */
+    public void service(
+        ServletRequest request,
+        ServletResponse response
+    ) throws ServletException, IOException {
+        String method = request.getMethod();
+        customLog("Sono il metodo public void service(ServletRequest, ServletResponse) [" + method + "] di Servlet1");
 
-		super.service(request, response);
-	}	
-	
-	/**
-	 **********************************
-	 **********************************
-	 * Metodi custom
-	 **********************************
-	 */
-	 
-	/*
-	 * Un handler comune che per ora non fa niente
-	 */
-	private void customLog(
-		String msessage
-	) {
-		/* Log4J per una roa cosi, anche no.*/
-		System.out.println(message);
-	}
-	 
-	/*
-	 * Un handler comune che per ora non fa niente
-	 */
-	private void doMyOp(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws IOException {
-		customLog("Sono il metodo private void doMyOp(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet1");
-		if (response != customLogl) {
-			PrintWriter out = response.getWriter();
-			out.write("<html><body><h4>Hai chiamato il metodo [" +  + "] di Servlet1!</h4></body></html>");
-		}
-	}
+        HttpServletRequest  request;
+        HttpServletResponse response;
+        
+        if (!(req instanceof HttpServletRequest &&
+                res instanceof HttpServletResponse)) {
+            throw new ServletException("Questa non era una richiesta o una risposta HTTP ...");
+        }
+
+        service(request, response);
+
+        /*
+            Ignoriamo il metodo standard cosi ci facciamo le richieste nostre
+            // super.service(request, response);
+        */
+    }    
+    
+    /**
+     **********************************
+     **********************************
+     * Metodi custom
+     **********************************
+     */
+     
+    /*
+     * Un handler comune che per ora non fa niente
+     */
+    private void customLog(
+        String msessage
+    ) {
+        /* Log4J per una roa cosi, anche no.*/
+        System.out.println(message);
+    }
+     
+    /*
+     * Un handler comune che per ora non fa niente
+     */
+    private void doMyOp(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws IOException {
+        customLog("Sono il metodo private void doMyOp(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet1");
+        if (response != customLogl) {
+            PrintWriter out = response.getWriter();
+            out.write("<html><body><h4>Hai chiamato il metodo [" +  + "] di Servlet1!</h4></body></html>");
+        }
+    }
 }
--- TestinaDiServlets/src/main/java/com/testina/servlet/Servlet3.java
+++ TestinaDiServlets/src/main/java/com/testina/servlet/Servlet3.java
@@ -22,215 +22,228 @@ import javax.servlet.http.HttpSession;
  *
  */
 public class Servlet3 extends HttpServlet {
-	private static final String MASSIMO3 = "Massimo3";
+    private static final String MASSIMO3 = "Massimo3";
 
-	/*
-	 **********************************
-	 **********************************
-	 * Metodi overrided
-	 **********************************
-	 */
+    /*
+     **********************************
+     **********************************
+     * Metodi overrided
+     **********************************
+     */
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Servlet3() {
-		customLog("Sono il costruttore public Servlet3() di Servlet3");
+        customLog("Sono il costruttore public Servlet3() di Servlet3");
     }
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void Init(
-		ServletConfig config
-	) throws ServletException {
-		customLog("Sono il costruttore public void Init(ServletConfig) di Servlet3");
-		super.init(config);
+    /**
+     * @see Servlet#init(ServletConfig)
+     */
+    public void Init(
+        ServletConfig config
+    ) throws ServletException {
+        customLog("Sono il costruttore public void Init(ServletConfig) di Servlet3");
+        super.init(config);
 
-		/* Eventual codice custom da qui in poi */
-	}
+        /* Eventual codice custom da qui in poi */
+    }
 
-	/**
-	 * @see Servlet#destroy()
-	 */
-	public void Destroy(
-	) {
-		customLog("Sono il distruttore public void Destroy() di Servlet3");
+    /**
+     * @see Servlet#destroy()
+     */
+    public void Destroy(
+    ) {
+        customLog("Sono il distruttore public void Destroy() di Servlet3");
 
-		/* Eventual codice custom da qui in poi */
-		super.destroy();
-	}
+        /* Eventual codice custom da qui in poi */
+        super.destroy();
+    }
 
-	/**
-	 * Processa la richiesta Head
-	 */
-	protected void doHead(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Head
+     */
+    protected void doHead(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Get
-	 */
-	protected void doGet(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Get
+     */
+    protected void doGet(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Post
-	 */
-	protected void doPost(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Post
+     */
+    protected void doPost(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Put
-	 */
-	protected void doPut(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Put
+     */
+    protected void doPut(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Delete
-	 */
-	protected void doDelete(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Delete
+     */
+    protected void doDelete(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Options
-	 */
-	protected void doOptions(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Options
+     */
+    protected void doOptions(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta Trace
-	 */
-	protected void doTrace(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws ServletException, IOException {
-		doMyOp(request, response);
-	}
+    /**
+     * Processa la richiesta Trace
+     */
+    protected void doTrace(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        doMyOp(request, response);
+    }
 
-	/**
-	 * Processa la richiesta LastModified
-	 * questa pare che non passi da service(..., ...)
-	 * quindi dovrebbe chiamarla direttamente l'engine del web server
-	 */
-	protected long getLastModified(
-		HttpServletRequest request
-	) {
-		doMyOp(request, null);
-		return 0;
-	}
-	
-	/**
-	 * Processa la richiesta all'internal service handler
-	 */
-	protected void service(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) {
-		/*
-			Ignoriamo il metodo standard cosi ci facciamo le richieste nostre
-			// super.service(request, response);
-		*/
-	    String method = request.getMethod();
-		customLog("Sono il metodo protected void service(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet3");
-		
-		switch (method) {
-			case METHOD_HEAD :
-				doHead(request, response);
-				break;
-			case METHOD_GET :
-				doGet(request, response);
-				break;
-			case METHOD_POST :
-				doPost(request, response);
-				break;
-			case METHOD_PUT :
-				doPut(request, response);
-				break;
-			case METHOD_DELETE :
-				doDelete(request, response);
-				break;
-			case METHOD_OPTIONS :
-				doDelete(request, response);
-				break;
-			case METHOD_TRACE :
-				doDelete(request, response);
-				break;
-			case METHOD_MASSIMO3 :
-				doDelete(request, response);
-				break;
-			default:
-				customLog("Sono il metodo protected void service(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet3 e ... questo metodo non mi piace!");
+    /**
+     * Processa la richiesta LastModified
+     * questa pare che non passi da service(..., ...)
+     * quindi dovrebbe chiamarla direttamente l'engine del web server
+     */
+    protected long getLastModified(
+        HttpServletRequest request
+    ) {
+        doMyOp(request, null);
+        return 0;
+    }
+    
+    /**
+     * Processa la richiesta all'internal service handler
+     */
+    protected void service(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        /*
+            Ignoriamo il metodo standard cosi ci facciamo le richieste nostre
+            // super.service(request, response);
+        */
+        String method = request.getMethod();
+        customLog("Sono il metodo protected void service(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet3");
+        
+        switch (method) {
+            case METHOD_HEAD :
+                doHead(request, response);
+                break;
+            case METHOD_GET :
+                doGet(request, response);
+                break;
+            case METHOD_POST :
+                doPost(request, response);
+                break;
+            case METHOD_PUT :
+                doPut(request, response);
+                break;
+            case METHOD_DELETE :
+                doDelete(request, response);
+                break;
+            case METHOD_OPTIONS :
+                doDelete(request, response);
+                break;
+            case METHOD_TRACE :
+                doDelete(request, response);
+                break;
+            case METHOD_MASSIMO3 :
+                doDelete(request, response);
+                break;
+            default:
+                customLog("Sono il metodo protected void service(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet3 e ... questo metodo non mi piace!");
 
-				resp.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED, errMsg);
-				break;
-		}
-	}
-	
-	/**
-	 * Processa la richiesta al public service handler
-	 */
-	public void service(
-		ServletRequest request,
-		ServletResponse response
-	) {
-	    String method = request.getMethod();
-		customLog("Sono il metodo public void service(ServletRequest, ServletResponse) [" + method + "] di Servlet3");
+                resp.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED, errMsg);
+                break;
+        }
+    }
+    
+    /**
+     * Processa la richiesta al public service handler
+     */
+    public void service(
+        ServletRequest request,
+        ServletResponse response
+    ) throws ServletException, IOException {
+        String method = request.getMethod();
+        customLog("Sono il metodo public void service(ServletRequest, ServletResponse) [" + method + "] di Servlet3");
 
-		super.service(request, response);
-	}	
-	
-	/**
-	 **********************************
-	 **********************************
-	 * Metodi custom
-	 **********************************
-	 */
-	 
-	/*
-	 * Un handler comune che per ora non fa niente
-	 */
-	private void customLog(
-		String msessage
-	) {
-		/* Log4J per una roa cosi, anche no.*/
-		System.out.println(message);
-	}
-	 
-	/*
-	 * Un handler comune che per ora non fa niente
-	 */
-	private void doMyOp(
-		HttpServletRequest request,
-		HttpServletResponse response
-	) throws IOException {
-		customLog("Sono il metodo private void doMyOp(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet3");
-		if (response != null) {
-			PrintWriter out = response.getWriter();
-			out.write("<html><body><h4>Hai chiamato il metodo [" +  + "] di Servlet13</h4></body></html>");
-		}
-	}
+        HttpServletRequest  request;
+        HttpServletResponse response;
+        
+        if (!(req instanceof HttpServletRequest &&
+                res instanceof HttpServletResponse)) {
+            throw new ServletException("Questa non era una richiesta o una risposta HTTP ...");
+        }
+
+        service(request, response);
+
+        /*
+            Ignoriamo il metodo standard cosi ci facciamo le richieste nostre
+            // super.service(request, response);
+        */
+    }    
+    
+    /**
+     **********************************
+     **********************************
+     * Metodi custom
+     **********************************
+     */
+     
+    /*
+     * Un handler comune che per ora non fa niente
+     */
+    private void customLog(
+        String msessage
+    ) {
+        /* Log4J per una roa cosi, anche no.*/
+        System.out.println(message);
+    }
+     
+    /*
+     * Un handler comune che per ora non fa niente
+     */
+    private void doMyOp(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws IOException {
+        customLog("Sono il metodo private void doMyOp(HttpServletRequest, HttpServletResponse) [" + method + "] di Servlet3");
+        if (response != null) {
+            PrintWriter out = response.getWriter();
+            out.write("<html><body><h4>Hai chiamato il metodo [" +  + "] di Servlet13</h4></body></html>");
+        }
+    }
 }
