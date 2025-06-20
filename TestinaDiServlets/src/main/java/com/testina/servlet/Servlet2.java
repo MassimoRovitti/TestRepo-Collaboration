5b42b4fff6c43c496d8ff28cde36a163f2d7eead
MassimoRovitti <33849488+MassimoRovitti@users.noreply.github.com>
2025-06-20 13:22:34 +0200

    qualche import in java - scrivere con notepad++ e dura...

--- TestinaDiServlets/src/main/java/com/testina/servlet/Servlet2.java
+++ TestinaDiServlets/src/main/java/com/testina/servlet/Servlet2.java
@@ -3,10 +3,11 @@ package com.testina.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
